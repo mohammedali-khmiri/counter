025b4ff5e4863a69904aa825910f144d92938ccd
medAli <khemirimohamedali7@gmail.com>
2023-01-17 01:58:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+//set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+	btn.addEventListener("click", function (e) {
+		const classOfBtn = e.currentTarget.classList;
+		console.log(classOfBtn);
+		if (classOfBtn.contains("decrease")) {
+			count--;
+		} else if (classOfBtn.contains("increase")) {
+			count++;
+		} else {
+			count = 0;
+		}
+		if (count > 0) {
+			value.style.color = "green";
+		}
+		if (count < 0) {
+			value.style.color = "red";
+		}
+		if (count === 0) {
+			value.style.color = "#222";
+		}
+
+		value.textContent = count;
+	});
+});
